6dacf2428933e6943b0c35b06c66ca45977984c4
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-07 03:58:08 +0800

    initial the client section and initial the project structure in the client

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+// import typeDefs and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
 
+// import environment variables and mongoose models
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+// connect to mlab database
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -20,6 +23,7 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch(errors => console.error(errors));
 
+// create Apollo/GraphQL server using typedefs, resolvers and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
