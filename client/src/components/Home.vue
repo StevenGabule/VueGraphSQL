460a213d86e6891b55de91b41c131d56e3f04e94
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-07 22:42:40 +0800

    dive in smart queries in vue components

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+    <div>
+        <h1>Home</h1>
+        <div v-if="$apollo.loading">Loading...</div>
+        <ul v-else v-for="post in getPosts" :key="post._id">
+            <li>
+                {{post.title}}
+                {{post.imageUrl}}
+                {{post.description}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import {gql} from 'apollo-boost'
+
     export default {
-      name: 'Home'
+        name: 'Home',
+
+        data() {
+          return {
+            posts: []
+          }
+        },
+
+        apollo: {
+            getPosts: {
+                query: gql`
+                  query  {
+                      getPosts {
+                          _id
+                          title
+                          imageUrl
+                          description
+                          likes
+                      }
+                  }
+              `,
+              result({data,loading}) {
+                  if (!loading) {
+                    this.posts = data.getPosts;
+                  }
+              }
+            }
+        }
     }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,23 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+const defaultClient = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+});
+
+const apolloProvider = new VueApollo({defaultClient});
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    apolloProvider,
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app');
