5d4aa7164170e266381966e77adc7934f1fd4c55
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-07 16:51:51 +0800

    add side navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,35 @@
 <template>
-    <v-app>
+    <v-app style="background-color: #E3E3EE">
+        <!-- Side Navigation -->
+        <v-navigation-drawer app temporary fixed v-model="sideNav">
+            <v-toolbar color="accent" dark flat>
+                <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
+                <router-link to="/" tag="span" style="cursor: pointer">
+                    <h1 class="title pl-3">VueShare</h1>
+                </router-link>
+            </v-toolbar>
+            <v-divider />
+            <!-- SideNavbar Links -->
+            <v-list>
+                <v-subheader>Navigation Links</v-subheader>
+                <v-list-item-group color="primary">
+                    <v-list-item v-for="item in sideNavItems" :key="item.title" :to="item.link">
+                        <v-list-item-icon>
+                            <v-icon v-text="item.icon"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title v-text="item.title"></v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
         <!-- Horizontal Navbar-->
         <v-card color="grey lighten-4" flat tile>
 
             <v-toolbar fixed color="primary" dark>
 
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
 
                 <v-toolbar-title>
                     <router-link to="/" tag="span" style="cursor: pointer">VueShare</router-link>
@@ -44,8 +68,21 @@
     export default {
         name: 'App',
 
+        data() {
+          return {
+              sideNav: false
+          }
+        },
+
         computed: {
             horizontalNavItems() {
+                return [
+                    {icon: 'mdi-chat', title: "Posts", link: '/posts'},
+                    {icon: 'mdi-lock-open', title: "Sign in", link: '/signin'},
+                    {icon: 'mdi-pencil', title: "Sign up", link: '/signup'},
+                ]
+            },
+            sideNavItems() {
                 return [
                     {icon: 'mdi-chat', title: "Posts", link: '/posts'},
                     {icon: 'mdi-lock-open', title: "Sign in", link: '/signin'},
