c2ea6c5333c3f287a86e670992e99368633c3c6b
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-07 23:33:23 +0800

    add carousel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-        <h1>Home</h1>
-        <div v-if="$apollo.loading">Loading...</div>
-        <ul v-else v-for="post in getPosts" :key="post._id">
-            <li>
-                {{post.title}}
-                {{post.imageUrl}}
-                {{post.description}}
-            </li>
-        </ul>
+    <div v-if="getPosts" class="text-center">
+        <v-flex xs12>
+            <v-carousel cycle interval="3000" hide-delimiter-background
+                        show-arrows-on-hover>
+                <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+                    <h1 id="carousel__title">{{ post.title }}</h1>
+                </v-carousel-item>
+            </v-carousel>
+        </v-flex>
     </div>
 </template>
 
@@ -17,13 +16,6 @@
 
     export default {
         name: 'Home',
-
-        data() {
-          return {
-            posts: []
-          }
-        },
-
         apollo: {
             getPosts: {
                 query: gql`
@@ -36,13 +28,22 @@
                           likes
                       }
                   }
-              `,
-              result({data,loading}) {
-                  if (!loading) {
-                    this.posts = data.getPosts;
-                  }
-              }
+              `
             }
         }
     }
 </script>
+
+<style>
+    #carousel__title {
+        position: absolute;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: white;
+        border-radius: 5px 5px 0 0;
+        margin: 0 auto;
+        padding: 0.5em 0.5em 40px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
