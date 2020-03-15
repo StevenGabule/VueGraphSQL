b04ba5c347a08256d2c976905d03e280d71b69f1
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-15 23:38:49 +0800

    build out post card in post component

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -1,24 +1,83 @@
 <template>
-  <v-container v-if="getPost">
-    <h1>{{ getPost.title}}</h1>
-  </v-container>
+    <v-container v-if="getPost" class="mt-3" flexbox center>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card hover>
+                    <v-card-title>
+                        <h1>{{getPost.title}}</h1>
+                        <v-btn large icon v-if="user">
+                            <v-icon large color="grey">mdi-heart</v-icon>
+                        </v-btn>
+                        <h3 class="ml-3 font-weight-thin">{{ getPost.likes}} Likes</h3>
+                        <v-spacer></v-spacer>
+                        <v-icon @click="gotoPreviousPage" color="info" large>mdi-arrow-left-thick</v-icon>
+                    </v-card-title>
+
+                    <v-tooltip right>
+                        <span>Click to enlarge image</span>
+                        <v-img @click="toggleImageDialog" slot="activator" :src="getPost.imageUrl" id="post__image" />
+                    </v-tooltip>
+                    <!-- enlarged post image dialog -->
+                    <v-dialog v-model="dialog">
+                        <v-card>
+                            <v-img :src="getPost.imageUrl" height="80vh" />
+                        </v-card>
+                    </v-dialog>
+                    <v-card-text>
+                        <span v-for="(category, index) in getPost.categories" :key="index">
+                            <v-chip class="mb-3" color="accent" text-color="white">{{ category }}</v-chip>
+                        </span>
+                        <h3>{{ getPost.description }}</h3>
+                    </v-card-text>
+                </v-card>
+
+            </v-flex>
+
+        </v-layout>
+
+    </v-container>
 </template>
 
 <script>
     import {GET_POST} from "../../store/queries";
+    import {mapGetters} from "vuex";
 
-export default {
-  name: "Post",
-  props: ["postId"],
-    apollo: {
-      getPost: {
-          query: GET_POST,
-          variables() {
-              return {
-                  postId: this.postId
-              }
-          }
-      }
-    }
-};
+    export default {
+        name: "Post",
+        props: ["postId"],
+        data() {
+            return {
+                dialog: false,
+            }
+        },
+        apollo: {
+            getPost: {
+                query: GET_POST,
+                variables() {
+                    return {
+                        postId: this.postId
+                    }
+                }
+            }
+        },
+        computed: {
+            ...mapGetters(['user']),
+        },
+        methods: {
+            gotoPreviousPage(){
+                this.$router.go(-1);
+            },
+            toggleImageDialog() {
+                if (window.innerWidth > 500) {
+                    this.dialog = !this.dialog;
+                }
+            }
+        }
+    };
 </script>
+
+<style scoped>
+    #post__image {
+        height: 400px !important;
+    }
+</style>
